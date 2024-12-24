2601de318ccf5026d58473281c9fba4611776645
RushirajRavalji <driger.ray.dranzer@gmail.com>
2024-12-24 13:19:50 +0530

    small updates to quiz app

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,24 +1,58 @@
+// Import the material package, which contains the material design widgets used in Flutter.
 import 'package:flutter/material.dart';
 
-// The initial screen of the app, which allows the user to start the quiz.
+// Define the StartScreen class as a StatelessWidget. This widget will be used
+// as the starting screen for the quiz.
 class StartScreen extends StatelessWidget {
-  const StartScreen(this.startQuiz, {Key? key}) : super(key: key);
+  // Constructor for StartScreen. It takes `startQuiz`, a callback function,
+  // and an optional `key` parameter. The `startQuiz` function will be executed
+  // when the user presses the button to start the quiz.
+  const StartScreen(this.startQuiz, {Key? key});
 
-  final void Function() startQuiz; // Callback function to start the quiz
+  // Declare a final variable `startQuiz` which holds a function that doesn't
+  // return anything and doesn't take any parameters (void Function()).
+  final void Function() startQuiz;
 
+  // The build method describes the UI structure for the StartScreen widget.
   @override
   Widget build(BuildContext context) {
+    // Return a Center widget to center the child widgets on the screen.
     return Center(
-      child: ElevatedButton(
-        onPressed: startQuiz, // Trigger the startQuiz callback when pressed
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.symmetric(
-              vertical: 12.0, horizontal: 24.0), // Button padding
-        ),
-        child: Text(
-          'Start Quiz', // Button text
-          style: TextStyle(fontSize: 18), // Button text styling
-        ),
+      child: Column(
+        // Align the children vertically in the center of the screen.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Display an image from the assets (in this case, a logo for the quiz).
+          Image.asset(
+            'assets/images/quiz-logo.png', // Path to the image file.
+            width: 250, // Set the width of the image.
+          ),
+          // Add some space between the image and the text (80 pixels).
+          SizedBox(height: 80),
+          // Display a text message ("Let's Learn Flutter") below the image.
+          Text(
+            "Let's Learn Flutter",
+            style: TextStyle(
+              color: Color.fromARGB(255, 33, 33, 33), // Set the text color.
+              fontSize: 16, // Set the font size.
+            ),
+          ),
+          // Add some space between the text and the button (30 pixels).
+          SizedBox(height: 30),
+          // Create an outlined button with an icon and label.
+          OutlinedButton.icon(
+            // Define the action for the button. The startQuiz function
+            // will be called when the button is pressed.
+            onPressed: startQuiz,
+            // Style the button with a purple accent color for the text and icon.
+            style:
+                OutlinedButton.styleFrom(foregroundColor: Colors.purpleAccent),
+            // Set the icon that will appear on the button (right arrow).
+            icon: Icon(Icons.arrow_right),
+            // Set the label of the button ("Let's Go").
+            label: Text("Let's Go"),
+          )
+        ],
       ),
     );
   }
--- rolldice/lib/gradient_container.dart
+++ rolldice/lib/gradient_container.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart'; // Importing Flutter's core UI components
 import 'package:rolldice/dice_roller.dart'; // Importing the diceRoller widget from roll_dice.dart
 
-// Alignment for the gradient start and end points
-var startAlignment =
-    Alignment.topLeft; // Gradient starts from the top-left corner
-var endAlignment =
-    Alignment.bottomRight; // Gradient ends at the bottom-right corner
-
 class GradiendContainer extends StatelessWidget {
   // Stateless widget because it does not change
   const GradiendContainer({super.key}); // Constructor for the widget
@@ -25,8 +19,8 @@ class GradiendContainer extends StatelessWidget {
             Color.fromARGB(255, 241, 188, 109), // Orange-yellow color
             Color.fromARGB(255, 241, 234, 109), // Light yellow color
           ],
-          begin: startAlignment, // Starting point of the gradient
-          end: endAlignment, // Ending point of the gradient
+          begin: Alignment.topLeft, // Starting point of the gradient
+          end: Alignment.bottomRight, // Ending point of the gradient
         ),
       ),
       child: Center(
