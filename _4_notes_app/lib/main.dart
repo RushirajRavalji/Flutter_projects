794eb7a9c3bd96858e146c4625503a1819457178
RushirajRavalji <driger.ray.dranzer@gmail.com>
2025-01-22 10:53:21 +0530

    create todo app

--- _4_notes_app/lib/main.dart
+++ _4_notes_app/lib/main.dart
@@ -1,61 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart';
-import 'providers/note_provider.dart';
-import 'screens/login_screen.dart';
-import 'screens/registration_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/create_note_screen.dart';
-import 'services/notification_service.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  NotificationService.initialize();
-  runApp(MyApp());
-}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => NoteProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.black,
-          scaffoldBackgroundColor: Colors.black,
-          cardColor: Colors.grey[900],
-          textTheme: TextTheme(
-            bodyText1: TextStyle(color: Colors.white),
-            bodyText2: TextStyle(color: Colors.grey[400]),
-          ),
-          appBarTheme: AppBarTheme(
-            backgroundColor: Colors.black,
-            elevation: 0,
-            iconTheme: IconThemeData(color: Colors.white),
-            titleTextStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Colors.grey[800],
-            foregroundColor: Colors.white,
-          ),
-        ),
-        initialRoute: '/login',
-        routes: {
-          '/login': (context) => LoginScreen(),
-          '/register': (context) => RegistrationScreen(),
-          '/home': (context) => HomeScreen(),
-          '/create-note': (context) => CreateNoteScreen(),
-        },
-      ),
-    );
+    return const Placeholder();
   }
 }
