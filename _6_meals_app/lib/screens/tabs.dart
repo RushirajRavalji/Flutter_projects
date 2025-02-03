943b5c6d6060dd0637c2b273e313e524489bbe7e
RushirajRavalji <driger.ray.dranzer@gmail.com>
2025-02-03 09:45:35 +0530

    add navigation bar

--- _6_meals_app/lib/screens/tabs.dart
+++ _6_meals_app/lib/screens/tabs.dart
@@ -1,3 +1,5 @@
+import 'package:_6_meals_app/screens/categories.dart';
+import 'package:_6_meals_app/screens/meals.dart';
 import 'package:flutter/material.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -8,8 +10,32 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+int _selectedPageIndex = 0;
+
+void _selectPage(int index){
+  setState(() {
+    _selectedPageIndex = index;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+Widget activePage = const CategoriesScreen();
+if(_selectedPageIndex == 1){
+  activePage = MealsScreen(title: 'Favourites', meals: [],);
+}
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dynamic..'),
+      ),
+      body: ,
+      bottomNavigationBar: BottomNavigationBar( 
+      onTap: _selectPage,
+      items:const [
+        BottomNavigationBarItem(icon: Icon(Icons.set_meal), label:'Categories' ),
+        BottomNavigationBarItem(icon: Icon(Icons.star), label:'Favourite' ),
+      ],),
+    );
   }
 }
